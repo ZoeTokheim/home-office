a66d79751883fbf9841af477def2fe5d5e10fee8
Zoe Tokheim <zoetokheim@gmail.com>
2023-04-27 13:17:57 -0700

    removed unnecessary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // Creating react app: https://create-react-app.dev/
 
-// import TimeBasedText from "./Home/TimeBasedText"
 import './App.css';
 import React, { useState } from 'react';
 import Computer from "./Computer/Computer";
@@ -8,16 +7,6 @@ import Window from "./Window/Window";
 import plant from './plant.png';
 
 function App() {
-  // const [showModal, setShowModal] = useState(true);
-  // const [entries, setEntries] = useState(
-  //   JSON.parse(localStorage.getItem('entries')) || []
-  // );
-  const [entry, setEntry] = useState(["", -1]);
-  // const [name, setName] = useState(
-  //   JSON.parse(localStorage.getItem('name')) || ""
-  // );
-  // const [tempName, setTempName] = useState(name);
-
   return (
     <div className="App">
         <Computer/>
